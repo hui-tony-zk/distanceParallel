8072689fc18dee20da3db2ec13c0156a0947f8fd
Tony Hui <hui.tony.zk@gmail.com>
2015-11-20 16:59:53 -0800

    fixed looping command

--- R/create_pairwise_comparisons.R
+++ R/create_pairwise_comparisons.R
@@ -10,7 +10,8 @@
 #' @examples
 #' matrix <- replicate(20, rnorm(10))
 #' create_pairwise_comparisons(matrix, ncores = 1) #not allowed to have parallel examples
-create_pairwise_comparisons <- function(matrix, ncores = 1, by = "row"){
+
+create_pairwise_comparisons <- function(matrix, ncores = 1, by = "row", distfun = function(x1, x2) sqrt(sum((x1 - x2) ^ 2))){
   if (!(is.matrix(matrix) | is.data.frame(matrix))) stop("'matrix' must be a matrix or data frame")
   if (ncores > parallel::detectCores()) {
     stop(paste("Warning! You specified", ncores, "cores, but we only detected", parallel::detectCores(), "cores."))
@@ -23,7 +24,7 @@ create_pairwise_comparisons <- function(matrix, ncores = 1, by = "row"){
   # Parallelize the dopar function by splitting the workload into batches for each core, since foreach is only good when number of jobs roughly equals number of cores
   task_list <- divide_cores(ncol(comb), ncores = ncores)
   # Combine
-  pairwise <- foreach::foreach(i=1:nrow(task_list), .combine = c) %dopar% {
+  pairwise <- foreach::foreach(i=1:nrow(task_list), .combine = rbind) %dopar% {
     start <- task_list[i,1]
     end <- task_list[i,2]
     merge_bind=vector("list", end-start+1)
@@ -38,12 +39,14 @@ create_pairwise_comparisons <- function(matrix, ncores = 1, by = "row"){
         sample1 <- matrix[,one]
         sample2 <- matrix[,two]
       }
-      merge_temp <- cbind(sample1, sample2)
-      colnames(merge_temp) <- c(one, two)
-      merge_bind[[f-start+1]] <- merge_temp
-      names(merge_bind)[f-start+1] <- paste0(one,"_",two)
+      diff_temp <- data.frame(
+        x = one,
+        y = two,
+        dist = distfun(sample1, sample2)
+      )
+      merge_bind[[f-start+1]] <- diff_temp
     }
-    merge_bind
+    data.table::rbindlist(merge_bind)
   }
-  return(pairwise)
+  return(as.data.frame(pairwise))
 }
