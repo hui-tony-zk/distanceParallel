3e8e748aa04df454de5c6fdc9a78474774420962
Tony Hui <hui.tony.zk@gmail.com>
2015-11-20 16:24:32 -0800

    added vingette

---
+++ R/distanceParallel.R
@@ -0,0 +1,7 @@
+#' Parallel distance matrix calculations
+#'
+#' dist(x) in base R only operates on one thread. Using the foreach package, we can now create distance matrixes in parallel.
+#'
+#' @docType package
+#' @name distanceParallel
+NULL
